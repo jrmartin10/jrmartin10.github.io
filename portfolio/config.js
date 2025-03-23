e8391fa6e86c537374464128fd73efb8fe52f47d
jrmartin10 <jrmartin10@yahoo.es>
2025-03-23 21:27:52 +0100

    Create config.js

---
+++ portfolio/config.js
@@ -0,0 +1,27 @@
+export const config = {
+    // Thumbnail dimensions
+    thumbnailWidth: 225,  // in pixels (reduced by 25% from 300)
+    thumbnailHeight: 150, // in pixels (reduced by 25% from 200)
+    thumbnailGap: 20,     // gap between thumbnails in pixels
+    
+    // Gradient background colors
+    gradientColors: {
+        start: '#0a192f',
+        end: '#3a6ea5'
+    },
+    
+    // YouTube embed URL
+    youtubeEmbedUrl: 'https://www.youtube.com/embed/eEACXNo-LXE',
+    
+    // Type C dimensions
+    typeCWidth: 130, // in pixels
+    typeCHeight: 94, // in pixels
+    
+    // Slider titles
+    sliderTitles: [
+        "@bellostudios",
+        "Shorts - Spirit Evidences",
+        "AI Shorts",
+        "@Paranormal_Parkour"
+    ]
+};
