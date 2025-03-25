c88dc4a2ad51148001bd10715ba3f2474f319fa4
jrmartin10 <jrmartin10@yahoo.es>
2025-03-25 17:00:19 +0100

    Add files via upload

---
+++ portfolio2/config.js
@@ -0,0 +1,27 @@
+export const config = {
+    // Thumbnail dimensions
+    thumbnailWidth: 225,  // in pixels (reduced by 25% from 300)
+    thumbnailHeight: 150, // in pixels (reduced by 25% from 200)
+    thumbnailGap: 20,     // gap between thumbnails in pixels
+    
+    // Gradient background colors
+    gradientColors: {
+        start: '#0a192f',
+        end: '#3a6ea5'
+    },
+    
+    // YouTube embed URL
+    youtubeEmbedUrl: 'https://www.youtube.com/embed/eEACXNo-LXE',
+    
+    // Type C dimensions
+    typeCWidth: 160, // in pixels (updated to 4:3 ratio)
+    typeCHeight: 120, // in pixels (updated to 4:3 ratio),
+    
+    // Slider titles
+    sliderTitles: [
+        "@Paranormal_Parkour",
+        "@bellostudios-apps",
+        "Shorts - Spirit Evidences",
+        "Whispers from Beyond"
+    ]
+};
